f4e8ce70234aeee5a156576168cce3cd14a1d0c9
suez54 <suweys.mahamud@gmail.com>
2022-09-07 23:28:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-	char myfunc[]="Programming is like building a multilingual puzzle\n";
-		puts(myfunc);
-	return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
