1627884d36004ea7f1ec47705e870a053d27b511
suez54 <suweys.mahamud@gmail.com>
2022-09-14 12:35:31 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints the times table of an integer starting with zero
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int x, j, mult;
+
+for (x = 0;x <= 9; x++)
+{
+putchar('0');
+putchar(',');
+putchar(' ');
+for (j = 1;j <= 9; j++)
+{
+mult = x * j;
+if ((mult / 10) > 0)
+{
+putchar((mult / 10) + '0');
+}
+else
+{
+putchar(' ');
+}
+putchar((mult % 10) + '0');
+if (j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+}
