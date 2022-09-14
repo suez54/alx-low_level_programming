a18a0824a678b2b05d74eb4d9cbe3f80b94ee387
suez54 <suweys.mahamud@gmail.com>
2022-09-14 10:46:06 -0700

    signs ,last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,11 @@ int print_last_digit(int a)
 int last = a % 10;
 if (last < 0)
 {
-last =last * -1;
+last = last * -1;
 _putchar(last + '0');
 return (last);
-}else 
+}
+else
 {
 _putchar(last + '0');
 return (last);
