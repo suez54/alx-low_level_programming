964b6e94ee551e7a2c9467b741762631b8183e07
suez54 <suweys.mahamud@gmail.com>
2022-09-14 13:06:51 -0700

    times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+ * print_times_table - Prints the times table of the input,
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+int x, j, mult;
+if (n >= 0 && n <= 15)
+{
+for (x = 0; x <= n; x++)
+{
+putchar('0');
+for (j = 1; j <= n; j++)
+{
+putchar(',');
+putchar(' ');
+mult = x * j;
+if (mult <= 99)
+{
+putchar(' ');
+}
+if (mult <= 9)
+{
+putchar(' ');
+}
+if (mult > 100)
+{
+putchar((mult / 100) + '0');
+putchar((mult / 10) % 10 + '0');
+}
+else if (mult <= 99 && mult >= 10)
+{
+putchar((mult / 10) + '0');
+}
+putchar((mult % 10) + '0');
+}
+putchar('\n');
+}
+}
+}
