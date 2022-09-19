657962014ee6f658cc5609f000cecc0881a3bdfd
suez54 <suweys.mahamud@gmail.com>
2022-09-19 10:20:33 -0700

    str puts

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string
+ * @s: my variable.
+ * Return: return the variable
+ */
+int _strlen(char *s)
+{
+int i;
+int j;
+
+for (i = 0, j = strlen(s); i < j; i++)
+{
+}
+return (i);
+}
