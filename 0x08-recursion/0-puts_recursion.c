28cbb5d55350c50c77d2a977522a3e511663ce6b
suez54 <suweys.mahamud@gmail.com>
2022-09-28 09:30:43 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,15 @@
 /**
  * _puts_recursion - fn to print string
  * @s: string to print
- * Return: void
+ * Return: void type
  */
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-putchar(*s);
-_puts_recursion(s + 1);
 putchar('\n');
 return;
 }
 putchar(*s);
-putchar('\n');
+_puts_recursion(s + 1);
 }
